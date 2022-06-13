58536233d15dda8c78174f9b4969754a6f8754fd
Jesus V <heyzeus.1096@gmail.com>
2022-06-13 16:48:39 -0700

    added picture

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,25 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <img
-        src="https://static.wixstatic.com/media/71546a_3f715f9bdae94883b91336d257fba2c5~mv2.gif"
-        alt=""
-        style={{
-          width: "406px",
-          height: "405px",
-          object: "cover",
-          position: "50% 50%",
-        }}
-      />
+      <a href="./about-me" target="_blank">
+        <img
+          src="https://static.wixstatic.com/media/71546a_3f715f9bdae94883b91336d257fba2c5~mv2.gif"
+          alt=""
+          style={{
+            width: "406px",
+            height: "405px",
+            object: "cover",
+            position: "50% 50%",
+            margin: "0 auto",
+            textAlign: "left",
+          }}
+        />
+      </a>
       <h1>Hi there!</h1>
-      <h1>My name is John and I'm a UX & UI designer.</h1>
+      <h1>
+        My name is John and I'm a
+        <span style={{color: "darkgreen"}}> UX & UI</span> designer.
+      </h1>
     </div>
   );
 };
