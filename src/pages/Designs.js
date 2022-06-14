3b9c69ee43c36603306ba6dd069f1782780f1e6e
Jesus V <heyzeus.1096@gmail.com>
2022-06-13 23:45:49 -0700

    Added images on Designs tab

--- src/pages/Designs.js
+++ src/pages/Designs.js
@@ -3,7 +3,210 @@ import React from "react";
 const Designs = () => {
   return (
     <div>
-      <h1>This is the Designs page.</h1>
+      <div>
+        <h1>Mobile UI Designs</h1>
+        <img
+          src="https://static.wixstatic.com/media/71546a_a199be5e445f46168aa771325a94e71b~mv2.png/v1/fill/w_217,h_160,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Group%201282.png"
+          alt="Group 1282.png"
+          style={{
+            width: "271px",
+            height: "200px",
+            objectFit: "cover",
+            objectPosition: "50% 50%",
+          }}
+        ></img>
+        <img
+          src="https://static.wixstatic.com/media/71546a_1ac49b1ac0924b1a9ee10bc5550f3ca0~mv2.png/v1/fill/w_216,h_161,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Group%201320.png"
+          alt="Group 1320.png"
+          style={{
+            width: "270px",
+            height: "201px",
+            objectFit: "cover",
+            objectPosition: "50% 50%",
+          }}
+        ></img>
+        <img
+          src="https://static.wixstatic.com/media/71546a_341f6555c47244afa60284cbb65cdb43~mv2.png/v1/fill/w_217,h_161,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Group%201267.png"
+          alt="Group 1267.png"
+          style={{
+            width: "270px",
+            height: "201px",
+            objectFit: "cover",
+            objectPosition: "50% 50%",
+          }}
+        ></img>
+        <img
+          src="https://static.wixstatic.com/media/71546a_ca1d8e485e3d43288df305535ecf6785~mv2.png/v1/fill/w_221,h_166,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Group%201208.png"
+          alt="Group 1208.png"
+          style={{
+            width: "276px",
+            height: "207px",
+            objectFit: "cover",
+            objectPosition: "50% 50%",
+          }}
+        ></img>
+        <img
+          src="https://static.wixstatic.com/media/71546a_ca1d8e485e3d43288df305535ecf6785~mv2.png/v1/fill/w_221,h_166,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Group%201208.png"
+          alt="Group 1208.png"
+          style={{
+            width: "276px",
+            height: "207px",
+            objectFit: "cover",
+            objectPosition: "50% 50%",
+          }}
+        ></img>
+        <img
+          src="https://static.wixstatic.com/media/71546a_c4dea5ef4b2540698c93d418712e0f73~mv2.png/v1/fill/w_217,h_161,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Group%201233.png"
+          alt="Group 1233.png"
+          style={{
+            width: "271px",
+            height: "201px",
+            objectFit: "cover",
+            objectPosition: "50% 50%",
+          }}
+        ></img>
+        <img
+          src="https://static.wixstatic.com/media/71546a_5e9b4691872940cdb7e2cdecc2dd485a~mv2.png/v1/fill/w_223,h_161,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Group%201325.png"
+          alt="Group 1325.png"
+          style={{
+            width: "279px",
+            height: "201px",
+            objectFit: "cover",
+            objectPosition: "50% 50%",
+          }}
+        ></img>
+        <img
+          src="https://static.wixstatic.com/media/71546a_5e9b4691872940cdb7e2cdecc2dd485a~mv2.png/v1/fill/w_223,h_161,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Group%201325.png"
+          alt="Group 1325.png"
+          style={{
+            width: "279px",
+            height: "201px",
+            objectFit: "cover",
+            objectPosition: "50% 50%",
+          }}
+        ></img>
+      </div>
+      <div>
+        <h1>Desktop UI Designs</h1>
+        <img
+          src="https://static.wixstatic.com/media/71546a_9632344796904a908d9d2c679da9ec16~mv2.png/v1/fill/w_210,h_373,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Home.png"
+          alt="Home.png"
+          style={{
+            width: "263px",
+            height: "466px",
+            objectFit: "cover",
+            objectPosition: "50% 50%",
+          }}
+        ></img>
+        <img
+          src="https://static.wixstatic.com/media/71546a_33a36fdffce742d49ba28a814ad44f6c~mv2.png/v1/fill/w_201,h_373,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Group%201158.png"
+          alt="Group 1158.png"
+          style={{
+            width: "251px",
+            height: "466px",
+            objectFit: "cover",
+            objectPosition: "50% 50%",
+          }}
+        ></img>
+        <img
+          src="https://static.wixstatic.com/media/71546a_1518053b08f14c9c9f5224f8f22797d9~mv2.gif"
+          alt="H321_Homepage_Teen_Desktop_1110.gif"
+          style={{
+            width: "320px",
+            height: "466px",
+            objectFit: "cover",
+            objectPosition: "50% 50%",
+          }}
+        ></img>
+        <img
+          src="https://static.wixstatic.com/media/71546a_e9f76a7fa1154e0fafec6c10cea4834f~mv2.png/v1/fill/w_216,h_373,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Group%201339.png"
+          alt="Group 1339.png"
+          style={{
+            width: "270px",
+            height: "466px",
+            objectFit: "cover",
+            objectPosition: "50% 50%",
+          }}
+        ></img>
+        <div>
+          <h1>Vector Art</h1>
+          <img
+            src="https://static.wixstatic.com/media/71546a_8f969f4d0b104ede8d27a5262ab196d9~mv2.png/v1/fill/w_269,h_202,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Asset%2071%403x.png"
+            alt="Asset 71@3x.png"
+            style={{
+              width: "336px",
+              height: "252px",
+              objectFit: "cover",
+              objectPosition: "50% 50%",
+            }}
+          ></img>
+          <img
+            src="https://static.wixstatic.com/media/71546a_ea88082ee72a4816af70b21031202a74~mv2.png/v1/fill/w_295,h_202,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Asset%202%403x.png"
+            alt="Asset 2@3x.png"
+            style={{
+              width: "369px",
+              height: "252px",
+              objectFit: "cover",
+              objectPosition: "50% 50%",
+            }}
+          ></img>
+          <img
+            src="https://static.wixstatic.com/media/71546a_bf1d550a1e5943c9b69548b1ce1d147c~mv2.png/v1/fill/w_262,h_202,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Asset%203%403x.png"
+            alt="Asset 3@3x.png"
+            style={{
+              width: "327px",
+              height: "252px",
+              objectFit: "cover",
+              objectPosition: "50% 50%",
+            }}
+          ></img>
+          <img
+            src="https://static.wixstatic.com/media/71546a_0a2eae95477a472cae8b466f086f6ac9~mv2.png/v1/fill/w_269,h_202,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Asset%20222%403x.png"
+            alt="Asset 222@3x.png"
+            style={{
+              width: "336px",
+              height: "252px",
+              objectFit: "cover",
+              objectPosition: "50% 50%",
+            }}
+          ></img>
+          <button>View more on Dribble</button>
+        </div>
+        <div>
+          <h1>Food Blog</h1>
+          <img
+            src="https://static.wixstatic.com/media/71546a_df65b703166c49f0b7c3819adef09c7d~mv2.png/v1/fill/w_216,h_266,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/1_9.png"
+            alt="1.9.png"
+            style={{
+              width: "270px",
+              height: "333px",
+              objectFit: "cover",
+              objectPosition: "50% 50%",
+            }}
+          ></img>
+          <img
+            src="https://static.wixstatic.com/media/71546a_0e4b0d1c00bc4ddea8fe0290d930acd4~mv2.png/v1/fill/w_217,h_266,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/1_1.png"
+            alt="1.1.png"
+            style={{
+              width: "271px",
+              height: "333px",
+              objectFit: "cover",
+              objectPosition: "50% 50%",
+            }}
+          ></img>
+          <img
+            src="https://static.wixstatic.com/media/71546a_4c4f8e1e189c460c97c34bff05104776~mv2.png/v1/fill/w_216,h_266,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/1_5.png"
+            alt="1.5.png"
+            style={{
+              width: "270px",
+              height: "333px",
+              objectFit: "cover",
+              objectPosition: "50% 50%",
+            }}
+          ></img>
+          <button>View more on Instagram</button>
+        </div>
+      </div>
     </div>
   );
 };
