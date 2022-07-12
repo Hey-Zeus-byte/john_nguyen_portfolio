06315c1210ab0a68fb2d63cadbe2265f36ddfb8e
Jesus V <heyzeus.1096@gmail.com>
2022-07-11 23:03:33 -0700

    Added styled components

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,25 @@
 import React from "react";
 import "./css/footer.css";
+import styled from "styled-components";
 
 const Footer = () => {
+  const EmailLink = styled.a`
+    data-auto-recognition: true;
+    text-decoration: none;
+    color: black;
+
+    &:hover {
+      color: red;
+    }
+  `;
+
   return (
-    <div id="footer">
+    <div className="footer">
       <p>GET IN TOUCH</p>
       <p>
-        <a data-auto-recognition="true" href="mailto:Johnuxui@gmail.com">
+        <EmailLink href="mailto:Johnuxui@gmail.com">
           Johnuxui@gmail.com
-        </a>
+        </EmailLink>
       </p>
       <img
         src="https://static.wixstatic.com/media/71546a_eb2b4226bfda49229e0bbd4b8ef1724c~mv2.png/v1/fill/w_25,h_25,al_c,lg_1,q_85,enc_auto/baseline_copyright_black_18dp.png"
@@ -22,9 +33,13 @@ const Footer = () => {
       ></img>
       <p>2021 John Nguyen</p>
       <p>Made In San Francisco, CA</p>
-      <div>
+      <div style={{border: "5px solid red"}}>
         <strong style={{fontSize: "20px"}}>LET'S CONNECT</strong>
-        <a href="https://www.linkedin.com/in/johnguyen/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/johnguyen/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img
             src="https://static.wixstatic.com/media/71546a_30d3b9b3fe8d48d18d77ef1230fec3fd~mv2.png/v1/fill/w_40,h_40,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Group%201372.png"
             alt="linkedin"
@@ -36,7 +51,11 @@ const Footer = () => {
             }}
           ></img>
         </a>
-        <a href="https://www.instagram.com/avocado.dude/" target="_blank">
+        <a
+          href="https://www.instagram.com/avocado.dude/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img
             src="https://static.wixstatic.com/media/71546a_a47dc43876ae43df9ca55a484d378d78~mv2.png/v1/fill/w_40,h_40,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Group%201370.png"
             alt="instagram"
@@ -48,7 +67,7 @@ const Footer = () => {
             }}
           ></img>
         </a>
-        <a href="https://twitter.com/johnuxui" target="_blank">
+        <a href="https://twitter.com/johnuxui" target="_blank" rel="noreferrer">
           <img
             src="https://static.wixstatic.com/media/71546a_dd2291a233814c3a93a48d8412da8af0~mv2.png/v1/fill/w_44,h_40,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Group%201371.png"
             alt="twitter"
@@ -60,7 +79,11 @@ const Footer = () => {
             }}
           ></img>
         </a>
-        <a href="https://dribbble.com/johnuxui" target="_blank">
+        <a
+          href="https://dribbble.com/johnuxui"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img
             src="https://static.wixstatic.com/media/71546a_83e4837ecf2c449090f39c3c210430d5~mv2.png/v1/fill/w_42,h_40,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Group%201373.png"
             alt="website"
