90f20a4a480baca6d3dd1a103f7bfbeb0db434a1
Jesus V <heyzeus.1096@gmail.com>
2022-11-03 22:21:21 -0700

    Just opened branch to start work later

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,29 +1,46 @@
 import React from "react";
+import { ContentWrapper } from "../components/ContentWrapper";
+import styled from "styled-components";
+
+const TitleFontType = styled.h4`
+  color: #919191;
+  font-size: 15px;
+  text-align: center;
+  @media only screen and (max-width: 768px) {
+    font-size: 18px;
+    margin-top: 80px;
+  }
+`;
+
+const PurpleBackgroundImg = styled.div`
+  display: block;
+  width: 100px;
+  height: 300px;
+  background-color: #b5bcf0;
+`;
 
 const AboutMe = () => {
   return (
-    <div>
+    <ContentWrapper>
+      <PurpleBackgroundImg />
+      <TitleFontType>My Story</TitleFontType>
       <div>
-        <h6>
-          <span>
-            <strong>Hello there, I'm John.</strong>
-          </span>
-          A food lover, blogger, amateur photographer, and designer in San
-          Francisco.
-          <p>
-            The shift in my career happened when I was contacted to redesign a
-            menu for a popular brunch restaurant, and saw how visual design can
-            solve real-world challenges at scale.
-            <p>
-              Coming from a background in both supply chain and design, I love
-              connecting the dots between user satisfaction, pain points, and
-              business goals. In my free time, I enjoy promoting local
-              businesses and restaurants through my food blog.
-            </p>
-          </p>
-        </h6>
+        Hello there, I'm John. A food lover, blogger, amateur photographer, and
+        designer in San Francisco.
       </div>
-    </div>
+      <div>
+        The shift in my career happened when I was contacted to redesign a menu
+        for a popular brunch restaurant, and saw how visual design can solve
+        real-world challenges at scale.
+      </div>
+      <div>
+        Coming from a background in both supply chain and design, I love
+        connecting the dots between user satisfaction, pain points, and business
+        goals. In my free time, I enjoy promoting local businesses and
+        restaurants through my food blog.
+      </div>
+      {/* Line Break Here */}
+    </ContentWrapper>
   );
 };
 
